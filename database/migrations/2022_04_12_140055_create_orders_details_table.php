db0ebc12d857ba9d0839eb33c3b153a068931a9c
Alejandro Goez <desarrolloagoez23@gmail.com>
2022-04-12 11:51:58 -0500

    feat:creation migration order and order detail

---
+++ database/migrations/2022_04_12_140055_create_orders_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('price');
+            $table->integer('quantity');
+            $table->foreign('product')->references('id')->on('products');
+            $table->foreign('order')->references('id')->on('orders');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_details');
+    }
+};
